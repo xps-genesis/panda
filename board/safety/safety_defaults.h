adaa8fbd6be688656f036f4c1cb5aa4bc4c71f3c
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-02-26 02:37:53 -0500

    FCA smart eps draft-1

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -1,18 +1,64 @@
-static void send_mdps_enable_speed(CAN_FIFOMailBox_TypeDef *to_fwd){
-  bool is_speed_unit_mph = GET_BYTE(to_fwd, 2) & 0x2;
-  
-  int mdps_cutoff_speed = is_speed_unit_mph ? 76 : 120;  // factor of 2 from dbc
-  
-  int veh_clu_speed = GET_BYTE(to_fwd, 1) | (GET_BYTE(to_fwd, 2) & 0x1) << 8;
-  
-  if (veh_clu_speed < mdps_cutoff_speed) {
-    to_fwd->RDLR &= 0xFFFE00FF;
-    to_fwd->RDLR |= mdps_cutoff_speed << 8;
+static uint8_t fca_compute_checksum(CAN_FIFOMailBox_TypeDef *to_push) {
+  /* This function does not want the checksum byte in the input data.
+  jeep chrysler canbus checksum from http://illmatics.com/Remote%20Car%20Hacking.pdf */
+  uint8_t checksum = 0xFF;
+  int len = GET_LEN(to_push);
+  for (int j = 0; j < (len - 1); j++) {
+    uint8_t shift = 0x80;
+    uint8_t curr = (uint8_t)GET_BYTE(to_push, j);
+    for (int i=0; i<8; i++) {
+      uint8_t bit_sum = curr & shift;
+      uint8_t temp_chk = checksum & 0x80U;
+      if (bit_sum != 0U) {
+        bit_sum = 0x1C;
+        if (temp_chk != 0U) {
+          bit_sum = 1;
+        }
+        checksum = checksum << 1;
+        temp_chk = checksum | 1U;
+        bit_sum ^= temp_chk;
+      } else {
+        if (temp_chk != 0U) {
+          bit_sum = 0x1D;
+        }
+        checksum = checksum << 1;
+        bit_sum ^= checksum;
+      }
+      checksum = bit_sum;
+      shift = shift >> 1;
+    }
   }
+  return ~checksum;
+}
+
+
+static void send_lkas_enable_speed(CAN_FIFOMailBox_TypeDef *to_fwd){
+
+  int crc;
+  int eps_cutoff_speed = 0x20 | 0x80 << 8;  //65kph  
+  int veh_speed = GET_BYTE(to_fwd, 4) | GET_BYTE(to_fwd, 5) << 8;
+  
+  if (veh_speed < eps_cutoff_speed) {
+    to_fwd->RDHR &= 0x00FF0000;  //clear speed and Checksum
+    to_fwd->RDHR |= eps_cutoff_speed;  //replace speed
+      
+    crc = fca_compute_checksum(to_fwd);
+    
+    to_fwd->RDHR |= (((crc << 8) << 8) << 8);   //replace Checksum
+  }
+  
 };
 
+bool is_op_active = false;
+
 int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
-  UNUSED(to_push);
+  int addr = GET_ADDR(to_push);
+  int bus_num = GET_BUS(to_push);
+  
+  if ((addr == 658) && (bus_num == 0))
+  {
+    is_op_active = GET_BYTE(to_push, 0) & 0x10;
+  }
   return true;
 }
 
@@ -42,8 +88,13 @@ static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   
   if (bus_num == 0) {
     bus_fwd = 2;
-    if (addr == 1265) {
-      send_mdps_enable_speed(to_fwd);
+    if (addr == 284) {
+      if (!is_op_active) {
+      }
+      else {
+        send_lkas_enable_speed(to_fwd);
+      }
+        
     }
   }
   if (bus_num == 2) {
