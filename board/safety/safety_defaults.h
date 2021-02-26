0d72e6e0a695ac08c63ebc8c4f4ec64cb05c7f79
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-02-26 18:55:25 -0500

    Update safety_defaults.h

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -61,7 +61,7 @@ static void send_steer_enable_speed(CAN_FIFOMailBox_TypeDef *to_fwd){
       
   crc = fca_compute_checksum(to_fwd);
     
-  to_fwd->RDHR |= (((crc << 8) << 8) << 8);   //replace Checksum
+  to_fwd->RDHR |= ((crc << 8) << 8) << 8);   //replace Checksum
 };
 
 static void send_apa_signature(CAN_FIFOMailBox_TypeDef *to_fwd){
