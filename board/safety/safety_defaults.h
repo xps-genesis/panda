c830f3a46e2b33e9182629880c308ce6d28dbce9
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-02-26 18:55:00 -0500

    Update safety_defaults.h

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -100,11 +100,12 @@ static void send_lkas_signature(CAN_FIFOMailBox_TypeDef *to_fwd){
   else {
     lkas_loops_counter = 0;
      if ((is_op_active) && (steer_type == 4)){
+      to_fwd->RDHR &= 0x000040;     
      }
   }
- // to_fwd->RDHR &= 0x00FF0000;  //clear everything except counter
+  to_fwd->RDHR &= 0x000000FF;  //clear everything except counter
   crc = fca_compute_checksum(to_fwd);    
-  to_fwd->RDHR |= (((crc << 8) << 8) << 8);   //replace Checksum
+  to_fwd->RDHR |= (((crc << 8) << 8);   //replace Checksum
 };
 
 int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
