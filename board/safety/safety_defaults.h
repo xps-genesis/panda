45318f19d1ce336666496345714bdc25db91ca10
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-02-28 13:29:06 -0500

    Update safety_defaults.h

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -94,7 +94,7 @@ int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     lkas_torq = ((GET_BYTE(to_push, 0) & 0x7) << 8) | GET_BYTE(to_push, 1);
   }
   
-  if ((addr == 678) && (bus_num == 0) && (steer_type == 3)) {
+  if ((addr == 678) && (bus_num == 0)) {
     steer_type = GET_BYTE(to_push, 6);
   }
   return true;
