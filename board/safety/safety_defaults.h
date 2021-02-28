505bc2b4a249a02e1666be412b332410d4a74087
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-02-27 20:26:18 -0500

    Update safety_defaults.h

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -55,7 +55,7 @@ static void send_steer_enable_speed(CAN_FIFOMailBox_TypeDef *to_fwd){
   }
   
   to_fwd->RDHR &= 0x00FF0000;  //clear speed and Checksum
-  to_fwd->RDHR |= eps_cutoff_speed;       //replace speed
+  to_fwd->RDHR |= veh_speed;       //replace speed
       
   crc = fca_compute_checksum(to_fwd);
     
